85f4bfe1fae36ffa38ebe1a022780b6f3a60fce8
Nikhil <nikhilcares22@gmail.com>
2021-05-30 13:18:35 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+  storedPosts=[];
+
+
+  onPostAdded(any:post){
+    this.storedPosts.push(post);
+  }
 }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component,Input } from "@angular/core"
+
+@Component({
+  selector:"app-post-list",
+  templateUrl:"./post-list.component.html",
+  styleUrls:["./post-list.component.css"]
+})
+export class PostListComponent {
+  @Input() posts:any = [];//not working
+  // posts:any=[]
+  // posts:Array<any>=[
+  //   // {title:"First Post",content:"This is the first post."},
+  //   // {title:"Angular Course",content:"This Course will cover the basics of angular and the instructor is Maxmillan."},
+  //   {title:"Third Post.",content:"The third post is this"},
+  // ]
+}
